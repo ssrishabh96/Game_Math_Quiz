ab4ea1f070698a65c3ad1597944409fa635bf286
Rishabh Agrawal <ss.rishabh96@gmail.com>
2017-05-23 18:23:46 +0530

    Initial commit

---
+++ app/src/main/java/gametut/googlelle/game_tut1/activity/Main2Activity.java
@@ -0,0 +1,266 @@
+package gametut.googlelle.game_tut1.activity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+import gametut.googlelle.game_tut1.R;
+
+public class Main2Activity extends AppCompatActivity implements View.OnClickListener {
+
+
+    SharedPreferences prefs;
+    SharedPreferences.Editor editor;
+    String dataName = "MyData";
+    String intName = "MyScore";
+    String intName1 = "MyLevel";
+
+    int defaultInt = 0;
+    int hiScore,hiLevel;
+
+    int currentScore = 0;
+    int currentLevel = 1;
+    int correctAnswer ;
+    Random randInt ;
+
+    String operandstr;
+
+    int answerGiven = 0;
+    TextView score, level;
+
+    Button a, b, c,d;
+    TextView operanda, operator, operandb;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(0,0,0,"hello");
+
+        return true;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        prefs=getSharedPreferences(dataName,MODE_PRIVATE);
+        editor = prefs.edit();
+        hiScore = prefs.getInt(intName, defaultInt);
+        hiLevel = prefs.getInt(intName1, defaultInt);
+        //Either load our string or
+        //if not available our default string
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        operanda = (TextView) findViewById(R.id.operanda);
+        operandb = (TextView) findViewById(R.id.operandb);
+        operator = (TextView) findViewById(R.id.operator);
+        score = (TextView) findViewById(R.id.score);
+        level = (TextView) findViewById(R.id.level);
+
+        a = (Button) findViewById(R.id.ansa);
+        b = (Button) findViewById(R.id.ansb);
+        c = (Button) findViewById(R.id.ansc);
+        d = (Button) findViewById(R.id.ansd);
+        a.setOnClickListener(this);
+        b.setOnClickListener(this);
+        c.setOnClickListener(this);
+        d.setOnClickListener(this);
+
+
+
+        randInt = new Random();
+        int opeartion = randInt.nextInt(3);
+
+        switch (opeartion) {
+            case 0:
+                operandstr="+";
+                break;
+
+            case 1:
+                operandstr="*";
+                break;
+
+            case 2:
+                operandstr="-";
+                break;
+        }
+        operanda.setText(operandstr);
+        setQuestion();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        finish();
+    }
+    
+    @Override
+    public void onClick(View view) {
+
+
+        answerGiven = 0;
+
+
+        switch (view.getId()) {
+            case R.id.ansa:
+
+                answerGiven = Integer.parseInt("" + a.getText());
+
+                break;
+            case R.id.ansb:
+
+                answerGiven = Integer.parseInt("" + b.getText());
+
+                break;
+
+            case R.id.ansc:
+
+                answerGiven = Integer.parseInt("" + c.getText());
+
+                break;
+            case R.id.ansd:
+
+                answerGiven = Integer.parseInt("" + d.getText());
+
+                break;
+
+        }
+
+        updateScoreAndLevel(answerGiven);
+
+    }
+
+    void setQuestion() {
+        int numberRange = currentLevel * 8;
+        int partA = randInt.nextInt(numberRange);
+        partA++;//don't want a zero value
+        int partB = randInt.nextInt(numberRange);
+        partB++;
+
+
+        int opeartion = randInt.nextInt(3);
+        switch (opeartion) {
+            case 0:
+                operator.setText("+");
+                operandstr="+";
+                correctAnswer = partA + partB;
+                break;
+
+            case 1:
+                operator.setText("*");
+                operandstr="*";
+                correctAnswer = partA * partB;
+                break;
+
+            case 2:
+                operator.setText("-");
+                operandstr="-";
+                correctAnswer = partA - partB;
+                break;
+        }
+
+        int wrongAnswer1 = correctAnswer - randInt.nextInt(35);
+        int wrongAnswer2 = correctAnswer + randInt.nextInt(45);
+        int wrongAnswer3 = correctAnswer + randInt.nextInt(105);
+
+        operanda.setText("" + partA);
+        operandb.setText("" + partB);
+
+        int buttonLayout = randInt.nextInt(4);
+        switch (buttonLayout) {
+            case 0:
+                a.setText("" + correctAnswer);
+                b.setText("" + wrongAnswer1);
+                c.setText("" + wrongAnswer3);
+                d.setText("" + wrongAnswer2);
+
+                break;
+            case 1:
+                a.setText("" + wrongAnswer1);
+                b.setText("" + wrongAnswer2);
+                c.setText("" + correctAnswer);
+                d.setText("" + wrongAnswer3);
+
+                break;
+            case 2:
+                a.setText("" + wrongAnswer3);
+                b.setText("" + correctAnswer);
+                c.setText("" + wrongAnswer2);
+                d.setText("" + wrongAnswer1);
+
+                break;
+            case 3:
+                a.setText("" + wrongAnswer1);
+                b.setText("" + wrongAnswer2);
+                c.setText("" + wrongAnswer3);
+                d.setText("" + correctAnswer);
+
+                break;
+        }
+
+
+
+
+    }
+
+    boolean isCorrect(int answerGiven) {
+
+        boolean correctTrueOrFalse;
+        if (answerGiven == correctAnswer) {
+           correctTrueOrFalse = true;
+        } else {
+            correctTrueOrFalse = false;
+
+        }
+            return correctTrueOrFalse;
+    }
+
+
+
+    void updateScoreAndLevel(int answerGiven){
+        if(isCorrect(answerGiven)){
+            for(int i = 1; i <= currentLevel; i++){
+                currentScore = currentScore + i;
+            }
+            currentLevel++;
+        }
+        else{
+
+            if(currentScore > hiScore) {
+                hiScore = currentScore;
+                hiLevel=currentLevel;
+                editor.putInt(intName, hiScore);
+                editor.putInt(intName1, hiLevel);
+                editor.commit();
+                Toast.makeText(getApplicationContext(), "New Hiscore",
+                        Toast.LENGTH_SHORT).show();
+            }
+            currentScore = 0;
+            currentLevel = 1;
+        }
+
+        score.setText("" + currentScore);
+        level.setText("" + currentLevel);
+        setQuestion();
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+
+    }
+}
+
---
+++ app/src/main/java/gametut/googlelle/game_tut1/activity/MainActivity.java
@@ -0,0 +1,143 @@
+package gametut.googlelle.game_tut1.activity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import gametut.googlelle.game_tut1.R;
+import gametut.googlelle.game_tut1.activity.Main2Activity;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText et;
+    SharedPreferences prefs;
+    SharedPreferences.Editor editor;
+    RelativeLayout showBackground_rl;
+
+    String dataName = "MyData";
+    String intName = "MyScore";
+    String intName1 = "MyLevel";
+    String unamedefault="username";
+    String uname;
+    Button save;
+    TextView tv;
+
+    private int height, width;
+    int defaultInt = 0;
+    //both activities can see this
+    public static int hiScore;
+    public static int hillevel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv=(TextView)findViewById(R.id.textView4);
+        et=(EditText) findViewById(R.id.username);
+        save=(Button) findViewById(R.id.savebtn);
+
+        showBackground_rl=(RelativeLayout) findViewById(R.id.showBackround);
+
+        Bitmap bitmap= BitmapFactory.decodeResource(getResources(),R.mipmap.numbers);
+
+        BitmapDrawable ob = new BitmapDrawable(getResources(), bitmap);
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            showBackground_rl.setBackground(ob);
+        }
+
+
+        prefs=getSharedPreferences(dataName,MODE_PRIVATE);
+        et.setVisibility(View.INVISIBLE);
+        save.setVisibility(View.INVISIBLE);
+
+        editor = prefs.edit();
+
+
+        prefs = getSharedPreferences(dataName, MODE_PRIVATE);
+
+        hillevel= prefs.getInt(intName1, defaultInt);
+        hiScore = prefs.getInt(intName, defaultInt);
+        uname=prefs.getString(unamedefault, unamedefault);
+        Toast.makeText(this,"Welcome "+uname,Toast.LENGTH_LONG).show();
+
+        tv.setText("HighScore: "+hiScore+" Level: "+hillevel);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(et.getVisibility()!=View.VISIBLE)
+                {
+                et.setVisibility(View.VISIBLE);
+                save.setVisibility(View.VISIBLE);}
+                else
+                {
+
+
+                        et.setVisibility(View.INVISIBLE);
+                        save.setVisibility(View.INVISIBLE);
+                }
+
+            }
+        });
+
+
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this,"Welcome "+uname,Toast.LENGTH_LONG).show();
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        finish();
+    }
+
+    public void start_play(View v)
+    {
+        Intent i=new Intent(this,Main2Activity.class);
+        startActivity(i);
+
+    }
+    public void save(View v)
+    {
+        uname=et.getText().toString();
+        if(uname.length()>1)
+        {  editor.putString(unamedefault,uname);
+            editor.commit();
+        }
+
+        et.setVisibility(View.INVISIBLE);
+        save.setVisibility(View.INVISIBLE);
+        Toast.makeText(this,"Name Changed successfully.",Toast.LENGTH_SHORT).show();
+
+    }
+
+}
